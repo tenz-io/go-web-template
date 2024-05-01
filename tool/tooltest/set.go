54fd70427d97ce72c2a13f96d51abbb3b87e201c
Eddie Tenz <tzz1002@gmail.com>
2024-05-01 12:57:37 +0800

    config

--- tool/tooltest/set.go
+++ tool/tooltest/set.go
@@ -5,6 +5,8 @@ import (
 	"log"
 
 	"github.com/tenz-io/gokit/cmd"
+
+	"go-web-template/internal/config"
 )
 
 var setCmd = &cmd.Command{
@@ -40,6 +42,12 @@ func set(c *cmd.Context) error {
 		return fmt.Errorf("val is empty")
 	}
 
+	cnf, err := cmd.GetConfig[*config.Config](c)
+	if err != nil {
+		return fmt.Errorf("get config error: %w", err)
+	}
+
+	log.Printf("config in command: %v\n", cnf)
 	log.Printf("set key: %s, val: %s\n", key, val)
 	return nil
 }
